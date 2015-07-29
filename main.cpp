078731a433bc6ecfb6760f040c93fe9211860d46
markuspg <markuspg@users.noreply.github.com>
2015-07-29 17:47:21 +0200

    Show warning message if the required number of arguments was not given.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "webcamdisplay.h"
 #include <QApplication>
+#include <QMessageBox>
+#include <QObject>
 
 int main( int argc, char *argv[] )
 {
     QApplication a{ argc, argv };
+
+    if ( argc != 2 ) {
+        QMessageBox::information(nullptr, QObject::tr("Wrong quantity of arguments"),
+                                 QObject::tr("The number of arguments passed was not valid."
+                                             " There is exactly one argument which may and must be given:"
+                                             " The url of the webcam to be displayed."));
+        return 0;
+    }
+
     MWWebcamDisplay w;
     w.show();
 
